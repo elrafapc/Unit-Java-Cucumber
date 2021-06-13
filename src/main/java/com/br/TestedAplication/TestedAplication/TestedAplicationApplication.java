97215d89b41436b1f6fea044a3a5990afb1038db
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-13 11:26:53 -0300

    Initializing Project

---
+++ src/main/java/com/br/TestedAplication/TestedAplication/TestedAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.br.TestedAplication.TestedAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/TestedAplication/TestedAplication/TestedAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.TestedAplication.TestedAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
